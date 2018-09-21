76c8ccef876d2cdc8e155d524b89d9869aa3a01b
bawiz <dakdag92@gmail.com>
2018-09-21 14:17:41 +0200

    changed searcher class to compoundsearch in script.js.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@
 var appID = "b3023b39";
 var appKey = "004db4860b0f19250fd50026cbf4057d";
 
-var searcher = new ConceptWikiSearch("https://beta.openphacts.org/2.1", appID, appKey);  
-var callback=function(success, status, response){  
-    searcher.parseResponse(response);
+var searcher = new CompoundSearch("https://beta.openphacts.org/2.1", appID, appKey);  
+var callback=function(success, status, response){
+    var classMembersResult = searcher.parseCompoundClassMembersResponse(response);
     alert(response);
     $("#hello").append(response);
-};  
-// success is 'true' or 'false', status is the http status code, response is the raw result which the parser function accepts  
-// response will be null in the case of errors  
-// limit to 20 results, species human (branch 4), with type set to compounds (uuid 07a800....)  
-searcher.byTag('Aspirin', '20', '4', '07a84994-e464-4bbf-812a-a4b96fa3d197', callback);
+};
+searcher.compoundClassMembers('http://purl.obolibrary.org/obo/CHEBI_24431', 1, 20, null, null, callback);
